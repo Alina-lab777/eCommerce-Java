e131856b1f94bbb1368a8fa6dfb0ba5f043a98eb
Alina-lab777 <elliekuch@gmail.com>
2022-12-27 15:16:10 -0500

    2nd commit with Project structure

--- src/test/java/JavaClass1/JavaClassOne.java
+++ src/test/java/JavaClass1/JavaClassOne.java
@@ -3,5 +3,20 @@ package JavaClass1;
 public class JavaClassOne {
     public static void main(String[] args) {
 
+        /**
+         * Java DataTypes:
+         *
+         *
+         *
+         *
+         *
+         *
+         *
+         *
+         **/
+
+
+
+
     }
 }
