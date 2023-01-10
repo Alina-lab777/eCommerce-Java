d8f2fffcadc00276d2f36bc281cc0167277c9aae
Alina-lab777 <elliekuch@gmail.com>
2023-01-10 11:42:07 -0500

    Class activities are added

---
+++ src/test/java/JavaClass1/JavaClassWork.java
@@ -0,0 +1,31 @@
+package JavaClass1;
+
+public class JavaClassWork {
+
+   /**
+    * Please  declare and assign these DataTypes accordingly to the values:
+    *
+    * byte
+    * short
+    * int
+    * long
+    * float
+    * double
+    * boolean
+    * char
+    *
+    * 12.00f
+    * 165
+    * 15.99d
+    * 1456.90l
+    * false
+    * a
+    * 4
+    *254
+    *
+    *
+    *
+    *
+    * */
+
+}
