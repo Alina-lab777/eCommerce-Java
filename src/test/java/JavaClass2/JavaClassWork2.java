a0fc2066a6029a9c4bffb4c702fc5d75e163d2a3
Alina-lab777 <elliekuch@gmail.com>
2023-01-20 06:40:00 -0500

    Java Class Work 2 - Arrays

---
+++ src/test/java/JavaClass2/JavaClassWork2.java
@@ -0,0 +1,23 @@
+package JavaClass2;
+
+public class JavaClassWork2 {
+    public static void main(String[] args) {
+
+
+        /**
+         *  Practice/declare/assign arrays[] for all  data types and print them by their Index#
+         *           * byte
+         *           * short
+         *           * int
+         *           * long
+         *           * float
+         *           * double
+         *           * boolean
+         *           * char
+         *           * String
+         *
+         * */
+
+
+    }
+}
--- src/test/java/JavaClass2/JavaHomeWork2.java
+++ src/test/java/JavaClass2/JavaHomeWork2.java
@@ -1,4 +1,66 @@
 package JavaClass2;
 
 public class JavaHomeWork2 {
+
+
+
+    /**
+     *HomeWork:
+     * Find out multiplication of these numbers 256 and 18 and print result to Console output with appropriate message;
+     * Find out division of these numbers 840 and 20 and print result to Console output with appropriate message;
+     * Find out modulus of these numbers 100 and 25 and print result to Console output with appropriate message;
+     * Find out modulus of these numbers 100 and 17 and print result to Console output with appropriate message;
+     *
+     * Int a = 15;
+     * Int b = 3;
+     * Find out if “a” is equals to “b”
+     * Find out if “a” is greater than  to “b”
+     * Find out if “a” is less than  to “b”
+     * Find out if “a” doesn’t equal  than  to “b”
+     * Find out if “a” is greater than  to “b”
+     * and print result to Console output with appropriate message;
+     *
+     * Int c = 20
+     * Find out if value of c is greater than 10 and less than 30;
+     * Find out if value of c is greater than 50 or less than 30;
+     * Find out if value of c is greater than 10 and less than 30 - reverse your program;
+     *
+     * Practice Pre Post Increments:
+     * int x = 4;
+     * int y = (++x) + 100;
+     *
+     * int x = 4;
+     * int y = (x++) + 100;
+     *
+     *
+     *
+     *
+     *
+     * Practice Pre Post decrement:
+     * int aa = 10;
+     * int bb = (--aa) + 100;
+     * System.out.println(bb);
+     *
+     * int aa = 10;
+     * int bb = (aa–) + 100;
+     * System.out.println(bb);
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     * */
+
+
+
+
+
+
 }
