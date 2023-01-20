4ca7567ed89cd801dc51c3f937a1b76889acae2d
uzeagle <seshkuvatov@gmail.com>
2023-01-20 06:07:31 -0500

    Replicating Java Class

--- src/test/java/JavaClass1/JavaHomeWork1.java
+++ src/test/java/JavaClass1/JavaHomeWork1.java
@@ -1,37 +1,86 @@
 package JavaClass1;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class JavaHomeWork1 {
+    public static void main(String[] args) {
+        System.out.println(5);
+
+        System.out.println(1000000);
+
+        System.out.println("Anything I Want");
+
+        System.out.println("Quotations");
+
+
+        byte somebyte = 10;
+        short someshort = 20;
+        int someint = 30;
+        long someLong = 40L;
+        float somelong = 50F;
+        double somedouble = 60.00;
+        boolean someBoolean = true;
+        char someChar = 'b';
+
+        System.out.println(somebyte);
+        System.out.println(someshort);
+        System.out.println(someint + " "+ somebyte + " "+someChar+ " "+someLong
+                + " "+someBoolean+ " "+somedouble+ " "+someshort);
+
+        String someString = "Just creating some String data type";
+        System.out.println(someString);
+        String[]someStrings = {"String1","String2","String3", "String4", "String5",};
+        /**
+         * String1 -- internally assigned to the Index 0
+         * String2 -- internally assigned to the Index 1
+         * String3 -- internally assigned to the Index 2
+         * String4 -- internally assigned to the Index 3
+         * String5 -- internally assigned to the Index 4
+         */
+        System.out.println(someStrings[2]);
+
+        System.out.println("******************");
+
+        System.out.println(someStrings[2]);
+
+        System.out.println("***********************");
+
+        System.out.println(someStrings);
+
+
+       byte[]someArrayByte = {2,3,4,5};
+
+       int[]someArrayInt = {150, 160, 170};
+
+
+
+
+
+
+
+    }
+
     /**
      * Practice declaring and assigning Java variables:
+     * To find out difference between primitive and non-primitive
+     * Declare and assign covered Data types
      *
      *
-     *To find out difference between primitive and non- primitive
-     *
-     *
-     *Declare and assign covered Data types
-     *
-     *
-     */
-
-
-
-    /**
      * GIT-GITHub Homework
+     * 1.Update your Project with latest changes (right top down blue arrow button)
+     * 2.Make any changes within JavaClassWork
+     * 3.Commit your changes with some message
+     * 4.Push your changes (right top up green button)
+     * 5.Instead master create Your new remote BranchName
+     * 6.Click on Push Button
+     * 7.Go to eCommerce repository and Request my review under "reviewers"
+     * 8.Create Pull request
+     * 9. Notify me that PR has been raised.
      *
-     *1.Update your Project with latest changes (right top down blue arrow button)
-     *2.Make any changes within JavaClassWork
-     *3.Commit your changes with some message
-     *4.Push your changes (right top up green button)
-     *5.Instead master create Your new remote BranchName
-     *6.Click on Push Button
-     *7.Go to eCommerce repository and Request my review under "reviewers"
-     *8.Create Pull request
-     *9. Notify me that PR has been raised.
      *
      *
-     * */
-
-    /***
      * Process with e-Commerce Project:
      *
      * 1. As per 1 Java Class ---> only 1 Remote Branch should be created and named as Ex:"JavaHomeWork1"
@@ -43,9 +92,93 @@ public class JavaHomeWork1 {
      *
      *
      *
+     *
+     *
+     *
+     * Package -->Import-->Class
+     *
+     * PIC
+     *
+     * pvsm ---> shortcut for calling main method or function
+     *
+     * All codes lines are written within method or function
+     *
+     * sout ---> shortcut for printing output;
+     *
+     * //---> single line comment
+     *
+     * If you want to comment out multiple lines of code
+     *
+     *
+     * Java Data Type:
+     * Primitive:
+     *
+     * byte --- can store numbers from -127 to 128
+     * short--- can store anything from - 320000 to 32000
+     * int --- can store -2 000 000 000 to 2 000 000 000
+     * long--- can store -9 000 000 000 000 000 to 9 000 000 000 000 , always identified  by "L"
+     * float--- can store fractional numbers and identified by "f"
+     * double--- only stores fractional numbers
+     * boolean--- checks whether statement "True" or "False"
+     * char---- single character in single quotation marks like "a"
+     *
+     *
+     *
+     * Non-primitive:
+     *
+     * String --- some combination of letters and words;
+     * Array --- Datatype variableName = [someValue1, someValue2, someValue3, someValue4]
+     * Classes --- all user created Classes;
+     *
+     *
+     *In Java there is position of values known as "index" and we can call certain Index number bypassing Index number
+     * Index starts from Zero
+     *
+     *
+     *
+     * Declare ---- writing certain Data type;
+     * Assign ---- creating certain value to certain date type ;
+     *
+     *
+     *  HOME WORK --- from 1st Java Lesson (Jan 18th/2023)
+     *      *
+     *      * Call main method!
+     *      * Practice declaring and assigning Declare and assign covered Data types:
+     *      * * byte
+     *      * * short
+     *      * * int
+     *      * * long
+     *      * * float
+     *      * * double
+     *      * * boolean
+     *      * * char
+     *      * * String
+     *      *
+     *      * Practice/declare/assign arrays[] for all  data types and print them by their Index#
+     *      * byte
+     *      * short
+     *      * int
+     *      * long
+     *      * float
+     *      * double
+     *      * boolean
+     *      * char
+     *      * String
+     *      *
+     *      *
+     *      * Print Everything and Run your first program!
+     *      *
+     *      *
      */
 
-
-
+    byte somebyte = 10;
+    short someshort = 20;
+    int someint = 30;
+    long someLong = 40L;
+    float somelong = 50F;
+    double somedouble = 60.00;
+    boolean someBoolean = true;
+    char someChar = 'b';
 
 }
+
