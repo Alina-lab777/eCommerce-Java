5519cc1f01a387933ed3a765db7f07ed4f5bff98
Alina-lab777 <elliekuch@gmail.com>
2023-01-27 07:43:18 -0500

    The topics and HomeWork from 5th Java Lesson

--- src/test/java/JavaClass5/JavaClassWork5.java
+++ src/test/java/JavaClass5/JavaClassWork5.java
@@ -1,4 +1,66 @@
 package JavaClass5;
 
 public class JavaClassWork5 {
+    public static void main(String[] args) {
+
+      /**
+       * startsWith() ---> checks if string starts with given prefix or combination of characters;
+       *                 ---> case sensitive;
+       *        return type ---> boolean;
+       * */
+
+       String str1 = "Finally Friday!";
+       boolean resultOfStartsWith = str1.startsWith("Fin");
+       System.out.println(resultOfStartsWith);
+
+
+
+       /**
+        * toLowerCase() ---> converts all String to lowercase;
+        *      return type String;
+        * */
+
+
+       String lowercaseString = str1.toLowerCase();
+       System.out.println(lowercaseString);
+
+
+
+       /**
+        * toUpperCase() ---> converts all String to Upper case;
+        *       return type String;
+        * */
+
+
+       String toUpperCaseString = str1.toUpperCase();
+       System.out.println(toUpperCaseString);
+
+
+       /**
+        * trim() ---> removes extra space at the beginning of String and ending (extra trailing spaces);
+        *    return type ---> String;
+        * */
+
+       String trimString = " I turn coffee into code ";
+       String afterTrimming = trimString.trim();
+       System.out.println(afterTrimming);
+
+
+        /***
+         * length() ---> finds the length of the String;
+         *      return type ---> int;
+         */
+
+        int theLengthOfStr = trimString.length();
+        System.out.println("The length of Str1 with trailing spaces : " + theLengthOfStr);
+
+
+        int theLengthOfStr2 = afterTrimming.length();
+        System.out.println("The length of Str1 without trailing spaces : " + theLengthOfStr2);
+
+
+
+
+
+    }
 }
--- src/test/java/JavaClass5/JavaHomeWork5.java
+++ src/test/java/JavaClass5/JavaHomeWork5.java
@@ -3,16 +3,7 @@ package JavaClass5;
 public class JavaHomeWork5 {
     public static void main(String[] args) {
 
-        String str = "This is last index of example";
-        int index = str.lastIndexOf("f");
-        System.out.println(index);
 
-       String dd = str.replaceAll("", " ");
-       System.out.println(dd);
-
-       String[] splittedString = str.split(" ");
-        System.out.println(splittedString[0]);
-        System.out.println(splittedString[3]);
 
     }
 
@@ -22,10 +13,24 @@ public class JavaHomeWork5 {
 /**
  * HomeWork for 5th Class:
  *
+ *   String s1 = " I'm a automation Engineer ";
+ *   String s2 = "Manual QA";
+ *   String s3 = “”;
+ *   String s4 = " I'm%a%automation%Engineer ";
  *
+ *  Check the length() of each String for s1 and s2,  print the result with appropriate message;
+ *           if (the Length of s1 > 20 characters){
+ *                sout("More than 20");
+ *           }else {
+ *                sout("Less than 20");
+ *                 }
+
+ *  Can use Ternary Operator and store result in  String variable;
+ * 
  *
- *
- *
- *
+ *  Convert String s1, s2, to UpperCase(),  print the result with appropriate message;
+ *  Convert String s1, s2, to LowerCase(),  print the result with appropriate message;
+ *  trim() the String s1, s4  print the result with appropriate message;
+ *  Check s1, s2, s3, s4 whether they startsWith() “I’m”,  print the result with appropriate message;
  *
  **/
