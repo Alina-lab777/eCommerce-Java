e9822ac69e4c11d716efbe960c952be796a28003
Alina-lab777 <elliekuch@gmail.com>
2023-01-30 15:23:31 -0500

    HomeWork 7-8 added

--- src/test/java/JavaClass7/JavaHomeWork7.java
+++ src/test/java/JavaClass7/JavaHomeWork7.java
@@ -1,4 +1,21 @@
 package JavaClass7;
 
 public class JavaHomeWork7 {
+
+
+    /**
+     *
+     * 90-100 Grade-A
+     *      System.out.println("Grade A");
+     * 80-89  Grade-B
+     *      System.out.println("Grade B");
+     * 70-79  Grade-C
+     *      System.out.println("Grade C");
+     * <70    Grade-D
+     *      System.out.println("Grade D");
+     *
+     *
+     **/
+
+
 }
--- src/test/java/JavaClass8/JavaHomeWork8.java
+++ src/test/java/JavaClass8/JavaHomeWork8.java
@@ -1,4 +1,30 @@
 package JavaClass8;
 
 public class JavaHomeWork8 {
+
+
+    /**
+     *  If your BMI is less than 18.5,
+     *  System.out.println("It falls within the underweight range.\nYou need to gain some weight");
+     *           If your BMI is 18.5 to 24.9,
+     *  System.out.println("It falls within the Healthy Weight range.\nKeep on eating healthy");
+     *           If your BMI is 25.0 to 29.9,
+     * 	System.out.println("It falls within the overweight range.\nYou have to exercise");
+     *           If your BMI is 30.0 or higher,
+     * 	System.out.println("It falls within the obese range.\nYou have to diet");
+     *
+     *
+     *      For example, a person who weighs 180 lbs. and is 5 feet and 5 inches tall (65 inches total)
+     *      would calculate their BMI in the following way:
+     *
+     *      180 x 703 = 126,540.
+     *      126,540 / 65 = 1,946.769.
+     *      1,946.769 / 65 = 29.95.
+     *      Their BMI would be 29.9.
+     *
+     *      double feetToInches = heightInFeet * 12 + heightInInches;
+     *
+     * */
+
+
 }
