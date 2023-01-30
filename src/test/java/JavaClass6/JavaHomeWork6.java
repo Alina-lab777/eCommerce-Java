7a73a245d96f7d13ea2724b6fc0f8fb8526c35f7
Alina-lab777 <elliekuch@gmail.com>
2023-01-30 08:05:23 -0500

    JavaHomeWork6

--- src/test/java/JavaClass6/JavaHomeWork6.java
+++ src/test/java/JavaClass6/JavaHomeWork6.java
@@ -1,4 +1,27 @@
 package JavaClass6;
 
 public class JavaHomeWork6 {
+    public static void main(String[] args) {
+
+    }
+
+
 }
+
+
+
+
+
+
+
+/**
+ *   String s1 = "";
+ *   String s2 = "Monday Beginning of Working Week";
+ *
+ *
+ *    Check if String s1 and s2 are empty(),  print the result with appropriate message;
+ *    Check lastIndexOf() the String s2,  print the result with appropriate message;
+ *    Replace all “e” characters in String s2 for character “z” and  print the result with appropriate message;
+ *    split() String s2 based on “ ” store in array[] of String and print each position based on Index[0];
+ *    With the help of substring() print the output “Working” from the s2;
+*/
