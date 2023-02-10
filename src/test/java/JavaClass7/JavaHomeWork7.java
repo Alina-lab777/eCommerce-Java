4540051bd86ae97e9b2d5b94ae0f7f57a9a568f4
uzeagle <seshkuvatov@gmail.com>
2023-02-10 05:38:00 -0500

    For Loop

--- src/test/java/JavaClass7/JavaHomeWork7.java
+++ src/test/java/JavaClass7/JavaHomeWork7.java
@@ -5,12 +5,7 @@ public class JavaHomeWork7 {
     public static void main(String[] args) {
 
         String CarModel = "Honda"; // we are getting from user, what don't know what he is going to provide;
-        String CarModel2 = "Toyota";
-        String CarModel3 = "Tesla";
-        String CarModel4 = "Lexus RX350";
-        String CarModel5 = "Porsche";
-        String CarModel6 = "Toyota RAV4";
-        String CarModel7 = "Ferrari";
+
 
         if (CarModel.equals("Honda") || CarModel.equals("Toyota")) {
             System.out.println("The Vehicle type is : ‘Sedan’ ");
@@ -24,78 +19,7 @@ public class JavaHomeWork7 {
             System.out.println("Unknown Vehicle type ");
         }
 
-        if (CarModel2.equalsIgnoreCase("Honda") || CarModel2.equalsIgnoreCase("Toyota")) {
-            System.out.println("The Vehicle type is : ‘Sedan’ ");
-        } else if (CarModel2.equalsIgnoreCase("Tesla") || CarModel2.equalsIgnoreCase("Kia EV9")) {
-            System.out.println("The Vehicle type is : ‘Electric’ ");
-        } else if (CarModel2.equalsIgnoreCase("Lexus RX350") || CarModel2.equalsIgnoreCase("Toyota RAV4")) {
-            System.out.println("The Vehicle type is : ‘SUV’ ");
-        } else if (CarModel2.equalsIgnoreCase("Porsche") || CarModel2.equalsIgnoreCase("Ferrari")) {
-            System.out.println("The Vehicle type is : ‘Sports Car’ ");
-        } else {
-            System.out.println("Unknown Vehicle type ");
-        }
 
-        if (CarModel3.equals("Honda") || CarModel3.equals("Toyota")) {
-            System.out.println("The Vehicle type is : ‘Sedan’ ");
-        } else if (CarModel3.equalsIgnoreCase("Tesla") || CarModel3.equalsIgnoreCase("Kia EV9")) {
-            System.out.println("The Vehicle type is : ‘Electric’ ");
-        } else if (CarModel3.equalsIgnoreCase("Lexus RX350") || CarModel3.equalsIgnoreCase("Toyota RAV4")) {
-            System.out.println("The Vehicle type is : ‘SUV’ ");
-        } else if (CarModel3.equalsIgnoreCase("Porsche") || CarModel3.equalsIgnoreCase("Ferrari")) {
-            System.out.println("The Vehicle type is : ‘Sports Car’ ");
-        } else {
-            System.out.println("Unknown Vehicle type ");
-        }
-
-        if (CarModel4.equals("Honda") || CarModel4.equals("Toyota")) {
-            System.out.println("The Vehicle type is : ‘Sedan’ ");
-        } else if (CarModel4.equalsIgnoreCase("Tesla") || CarModel4.equalsIgnoreCase("Kia EV9")) {
-            System.out.println("The Vehicle type is : ‘Electric’ ");
-        } else if (CarModel4.equals("Lexus RX350") || CarModel4.equals("Toyota RAV4")) {
-            System.out.println("The Vehicle type is : ‘SUV’ ");
-        } else if (CarModel4.equalsIgnoreCase("Porsche") || CarModel4.equalsIgnoreCase("Ferrari")) {
-            System.out.println("The Vehicle type is : ‘Sports Car’ ");
-        } else {
-            System.out.println("Unknown Vehicle type ");
-        }
-
-
-        if (CarModel5.equals("Honda") || CarModel5.equals("Toyota")) {
-            System.out.println("The Vehicle type is : ‘Sedan’ ");
-        } else if (CarModel5.equalsIgnoreCase("Tesla") || CarModel5.equalsIgnoreCase("Kia EV9")) {
-            System.out.println("The Vehicle type is : ‘Electric’ ");
-        } else if (CarModel5.equalsIgnoreCase("Lexus RX350") || CarModel5.equalsIgnoreCase("Toyota RAV4")) {
-            System.out.println("The Vehicle type is : ‘SUV’ ");
-        } else if (CarModel5.equalsIgnoreCase("Porsche") || CarModel5.equalsIgnoreCase("Ferrari")) {
-            System.out.println("The Vehicle type is : ‘Sports Car’ ");
-        } else {
-            System.out.println("Unknown Vehicle type ");
-        }
-
-        if (CarModel6.equals("Honda") || CarModel6.equals("Toyota")) {
-            System.out.println("The Vehicle type is : ‘Sedan’ ");
-        } else if (CarModel6.equalsIgnoreCase("Tesla") || CarModel6.equalsIgnoreCase("Kia EV9")) {
-            System.out.println("The Vehicle type is : ‘Electric’ ");
-        } else if (CarModel6.equalsIgnoreCase("Lexus RX350") || CarModel6.equalsIgnoreCase("Toyota RAV4")) {
-            System.out.println("The Vehicle type is : ‘SUV’ ");
-        } else if (CarModel6.equalsIgnoreCase("Porsche") || CarModel6.equalsIgnoreCase("Ferrari")) {
-            System.out.println("The Vehicle type is : ‘Sports Car’ ");
-        } else {
-            System.out.println("Unknown Vehicle type ");
-        }
-
-        if (CarModel7.equals("Honda") || CarModel7.equals("Toyota")) {
-            System.out.println("The Vehicle type is : ‘Sedan’ ");
-        } else if (CarModel7.equalsIgnoreCase("Tesla") || CarModel7.equalsIgnoreCase("Kia EV9")) {
-            System.out.println("The Vehicle type is : ‘Electric’ ");
-        } else if (CarModel7.equalsIgnoreCase("Lexus RX350") || CarModel7.equalsIgnoreCase("Toyota RAV4")) {
-            System.out.println("The Vehicle type is : ‘SUV’ ");
-        } else if (CarModel7.equalsIgnoreCase("Porsche") || CarModel7.equalsIgnoreCase("Ferrari")) {
-            System.out.println("The Vehicle type is : ‘Sports Car’ ");
-        } else {
-            System.out.println("Unknown Vehicle type ");
-        }
 
         int Grade = 60;
 
