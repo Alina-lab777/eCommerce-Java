90a083edf56fd96585e2d83674922432646458d7
Alina-lab777 <elliekuch@gmail.com>
2023-02-15 08:20:44 -0500

    new chnages after merge

--- src/test/java/JavaClass14/JavaHomeWork14.java
+++ src/test/java/JavaClass14/JavaHomeWork14.java
@@ -1,4 +1,38 @@
 package JavaClass14;
 
 public class JavaHomeWork14 {
+    public static void main(String[] args) {
+
+        /**
+         * 1. Write the Fibonacci series program (next number is the sum of previous two numbers
+         * for example 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 etc.
+         * The first two numbers of fibonacci series are 0 and 1)
+         *
+         *  Use this flow:
+         *      1. Create int num1 = 0; int num2 = 1; int num3; int count = 10;
+         *      2. Start Your Loop starting from 0 and less than count;
+         *      3. num3 = num1 + num2;
+         *      4. sout it here;
+         *      5. In order to see added values at the next index, do some magic with reassigning num1,num2,num3;
+         *      6. Just think;
+         *
+         * 2. Print the biggest number in Array of int
+         *      Example: int[] arr = {15,150,25,40,57,80}
+         *      Output should be: Biggest number in given array: 150
+         *
+         *
+         * 3. Print the smallest number in Array of int
+         *      Example: int[] arr = {112,150,25,40,57,80}
+         *      Output should be: Smallest number in given array: 25
+         *
+         *
+         **/
+
+
+
+
+
+
+
+    }
 }
