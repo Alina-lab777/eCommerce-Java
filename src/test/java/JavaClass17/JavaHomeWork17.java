5c9592c6ae7e8a510cdcc7515d96f8d3e1130f81
uzeagle <seshkuvatov@gmail.com>
2023-02-27 04:22:09 -0500

    Constructor

--- src/test/java/JavaClass17/JavaHomeWork17.java
+++ src/test/java/JavaClass17/JavaHomeWork17.java
@@ -19,11 +19,91 @@ public class JavaHomeWork17 {
  **/
 
 
-public static void main(String[] args) {
+    /**
+     * Java Constructor ---> it is a special method, that it used to initialize the Object of a Class.
+     *                       (is called once the Object of a class is created);
+     *
+     **/
 
-    // Have to pass along :
-    Student sd = new Student();
+
+    /**
+     * Rules for Creating Constructor :
+     *                      1. The same name as a Class name;
+     *                      2. Has no return type;
+     *                      3. Can't be abstract, final, static;
+     **/
+
+
+    /**
+     * Types of Constructor :
+     *                      1. No argument/Default Constructor;
+     *                         Gets called once the Object of a Class is created.
+     *                         Example : JavaClassWork17 class17 = new JavaClassWork17();
+     *                                   ---> JavaClassWork17 (behind the scene Java creates default Constructor);
+     *                                   ---> new JavaClassWork17() is the new Object;
+     *
+     *                      2. Parameterized Constructor ---> consists of a name and parameter list (number of Input parameters,
+     *                                                                                               types of Input Parameters,
+     *                                                                                               order of Input Parameters)
+     **/
+
+
+    /**
+     * Private Constructor is defined if we want to limit a user from creating the Object of a Class;
+     *
+     **/
+
+
+    /**
+     * Constructor Overloading can be achieved when there are 2 or more Constructors (which have the same name as a Class name)
+     * but different Input Parameters;
+     **/
+
+    String className;
+    int classId;
+    int classSSN;
+
+
+    //No-arg/Default Constructor:
+    JavaHomeWork17() {
+    }
+
+    // Parameterized Constructor;
+    JavaHomeWork17(String constructorName, int constructorId) {
+        this.className = constructorName;
+        this.classId = constructorId;
+    }
+
+    // Parameterized Constructor;
+    JavaHomeWork17(String userName, int userId, int userSsn) {
+        this.className = userName;
+        this.classId = userId;
+        this.classSSN = userSsn;
+    }
+
+    // this keyword is used to set Field variables to method/constructor parameters;
+
+
+    public static void main(String[] args) {
+
+        JavaClassWork17 j17 = new JavaClassWork17();
+
+        // new JavaClassWork17(); --- we are creating new copy of a certain Class;
+        // JavaClassWork17 --- constructor (similar of a which exactly Class copy we want to get);
+
+
+        JavaClassWork17 class17 = new JavaClassWork17("Jane", 1122);
+        System.out.println(class17.className + " : " + class17.classId);
+
+        JavaClassWork17 class17_2 = new JavaClassWork17();
+
+        JavaClassWork17 class17_3 = new JavaClassWork17("John", 2323, 1234567);
+        System.out.println("Just created user : " + class17_3.className + " " + class17_3.classId + " " + class17_3.classSSN);
 
 
     }
 }
+   /* public static void main(String[] args) {
+
+        // Have to pass along :
+        Student sd = new Student();*/
--- src/test/java/JavaClass17/Student.java
+++ src/test/java/JavaClass17/Student.java
@@ -2,14 +2,38 @@ package JavaClass17;
 
 public class Student {
 
+    String studentFirstName;
+    String studentLastName;
+    String studentHomeCity;
+    String studentUsState;
 
+    int studentAge;
+    int studentId;
+    int studentCellNumber;
 
-
-    Student(){
+    Student(String firstName, String lastName, int age, int id) {
+        this.studentFirstName = firstName;
+        this.studentLastName = lastName;
+        this.studentAge = age;
+        this.studentId = id;
     }
 
+    Student(String homeCity, String usSate, int cellNumber) {
+        this.studentHomeCity = homeCity;
+        this.studentUsState = usSate;
+        this.studentCellNumber = cellNumber;
+    }
 
+    public static void main(String[] args) {
 
+        Student student1 = new Student("Brooklyn", "NY", 214235343);
+        System.out.println(student1.studentHomeCity + " " + student1.studentUsState + " " + student1.studentCellNumber);
 
+        Student student2 = new Student("Lolita", "Baku", 23, 212324);
+        System.out.println(student2.studentFirstName + " " + student2.studentLastName + " " + student2.studentAge + " " + student2.studentId);
 
+    }
 }
+
+
+
