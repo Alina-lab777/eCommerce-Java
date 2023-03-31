0c02f96323c48f3ed4c2f9e08cac10fc504f7f1d
Alina-lab777 <elliekuch@gmail.com>
2023-03-31 09:36:58 -0400

    GitBash fixed

--- src/test/java/JavaClass20/Inheritance/SubClass.java
+++ src/test/java/JavaClass20/Inheritance/SubClass.java
@@ -3,9 +3,6 @@ package JavaClass20.Inheritance;
 public class SubClass extends SuperClass{
 
 
-
-
-
     public boolean isStringIdentical (String str1, String str2){
         System.out.println("Checking in Child/Sub Class");
         return (str1.equalsIgnoreCase(str2));
