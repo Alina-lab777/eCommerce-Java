38f75407fa6abc5d7539797d91f016f23d87f653
Alina-lab777 <elliekuch@gmail.com>
2023-04-10 08:46:01 -0400

    HashSet-2

--- src/test/java/JavaClass24/JavaClassWork24.java
+++ src/test/java/JavaClass24/JavaClassWork24.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class JavaClassWork24 {
 
     /**
-     *  Arraylist stores PRIMITIVE DataType values. We have to define the datatype as a "wrapper class":
+     *  Arraylist stores NON-PRIMITIVE DataType values. We have to define the datatype as a "wrapper class":
      *  (wrapper class --- when non-primitive dataTypes starts with Capital Letter)
      *
      *  byte - Byte;
